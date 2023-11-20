44e084926fab22255ebd363ee58dec1ea1299e1c
Randall Coding <randallcoding@protonmail.com>
2023-11-20 18:12:38 +0700

    Replaces container with service for mariadb

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-
 acorn build -t opensupports && \
-acorn run -s opensupports:opensupports \
-   -v config,size=1Gi -v db,size=5Gi  \
+acorn run -s mariadb-user:mariadb-user \
+   -s opensupports:opensupports \
    -n open-supports opensupports
+
+# acorn run -s opensupports:opensupports \
+#    -v config,size=1Gi -v db,size=5Gi  \
+#    -n open-supports opensupports
